cb629284360484ac469a149f37e4bad3d4683225
Daniel Miranda <danielkza2@gmail.com>
2012-06-18 20:28:55 -0300

    Commmit for simple phase 2

--- game/VisibleEnt.c
+++ game/VisibleEnt.c
@@ -4,6 +4,8 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
+#include "assets.h"
+
 #include "ent.h"
 #include "VisibleEnt.h"
 
@@ -16,14 +18,17 @@ void VisibleEnt_m_init(Ent *ent)
 {
     VisibleEnt *v_ent = (VisibleEnt*)ent;
     SDL_Rect *rect = &v_ent->image_rect;
-        
-    v_ent->image = IMG_Load("./images/test.png");
+    const asset_data_t *test_image = assets_load(ASSET_TYPE_SURFACE,
+		                                         "images/boat.tga");
+
+	v_ent->image = test_image->surface;
     
     rect->x = rect->y = 0;
     if(v_ent->image != NULL) {
         rect->w = v_ent->image->w;
         rect->h = v_ent->image->h;
     } else {
+		printf("IMG_Load: %s\n", IMG_GetError());
         rect->w = rect->h = 0;
     }
 
@@ -44,6 +49,26 @@ void VisibleEnt_m_destroy(Ent *ent)
 void VisibleEnt_m_on_frame(Ent *ent)
 {
     VisibleEnt *v_ent = (VisibleEnt *)ent;
+	Uint8 *key_state = SDL_GetKeyState(NULL);
+	vec2 move_direction = {0, 0};
+
+	if(key_state[SDLK_LEFT]) {
+		move_direction.x += -1;
+	} else if(key_state[SDLK_RIGHT]) {
+		move_direction.x += +1;
+	}
+	
+	if(key_state[SDLK_UP]) {
+		move_direction.y += -1;
+	} else if(key_state[SDLK_DOWN]) {
+		move_direction.y += +1;
+	}
+
+	vec2_norm(&move_direction);
+	ent->move_direction = move_direction;
+	ent->speed = 250;
+
+	Ent_m_on_frame(ent);
 
     VisibleEnt_CALL(draw, v_ent);
 }
@@ -59,11 +84,11 @@ void VisibleEnt_m_draw(VisibleEnt *ent)
 
     position = Ent_GET(position, ent);
 
-    printf("Drawing VisibleEnt at position %d, %d with image %p\n",
+    printf("Drawing VisibleEnt at position %f, %f with image %p\n",
            position->x, position->y, image);
 
-    dest.x = position->x;
-    dest.y = position->y;
+    dest.x = (int)position->x;
+    dest.y = (int)position->y;
 
     screen = SDL_GetVideoSurface();
     SDL_BlitSurface(image, &ent->image_rect, screen, &dest);

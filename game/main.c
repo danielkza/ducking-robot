5988a6003fd580141621762f2aac1a590efcf266
Daniel Miranda <danielkza2@gmail.com>
2012-07-17 20:38:16 -0400

    Begin work on proper main loop, code reorg.

--- game/main.c
+++ game/main.c
@@ -5,107 +5,198 @@
 #include <SDL_main.h>
 
 #include "assets.h"
+#include "ent_table.h"
 
 #include "Boat.h"
 
-void reset_stdout()
-{    
-#ifdef WIN32
-    FILE * ctt = fopen("CON", "w" );
-    freopen( "CON", "w", stdout );
-    freopen( "CON", "w", stderr );
-#endif
-}
-
 #define FRAME_RATE_MAX 60
 
-int main(int argc, char **argv)
+void systems_init()
 {
-    SDL_Surface *screen, *background;
-    const SDL_VideoInfo *video_info;
-    Boat *boat, *boat2;
-    const asset_data_t *bg_asset;
-    Uint32 frame_start, frame_end = 0;
-
     SDL_Init(SDL_INIT_EVERYTHING);
     atexit(SDL_Quit);
 
-    screen = SDL_SetVideoMode(800, 600, 0, SDL_HWSURFACE|SDL_DOUBLEBUF);
-    reset_stdout();
+    assets_init();
+    ent_table_init();
+}
 
-    bg_asset = assets_load(ASSET_TYPE_SURFACE, "images/water.tga");
-    background = bg_asset->surface;
+void systems_shutdown()
+{
+    ent_table_shutdown();
+    assets_shutdown();
+}
+
+void create_entities()
+{
+    vec2 position;
+    Boat *boat, *boat2;
+    Ent *wall_top, *wall_bot, *wall_left, *wall_right;
+
+    position.x = 100;
+    position.y = 100;
+    wall_top = ENT_CREATE(Ent);
+    Ent_SET(flags, wall_top, EFLAGS_TOUCHABLE | EFLAGS_SOLID);
+    Ent_SET(position, wall_top, &position);
+    Ent_SET(bounds_width, wall_top, 250);
+    Ent_SET(bounds_height, wall_top, 1);
+    Ent_CALL(spawn, wall_top);
+    
+    position.x = 100;
+    position.y = 350;
+    wall_bot = ENT_CREATE(Ent);
+    Ent_SET(flags, wall_bot, EFLAGS_TOUCHABLE | EFLAGS_SOLID);
+    Ent_SET(position, wall_bot, &position);
+    Ent_SET(bounds_width, wall_bot, 250);
+    Ent_SET(bounds_height, wall_bot, 1);
+    Ent_CALL(spawn, wall_bot);
+
+    position.x = 100;
+    position.y = 100;
+    wall_left = ENT_CREATE(Ent);
+    Ent_SET(flags, wall_left, EFLAGS_TOUCHABLE | EFLAGS_SOLID);
+    Ent_SET(position, wall_left, &position);
+    Ent_SET(bounds_width, wall_left, 1);
+    Ent_SET(bounds_height, wall_left, 250);
+    Ent_CALL(spawn, wall_left);
+
+    position.x = 350;
+    position.y = 100;
+    wall_right = ENT_CREATE(Ent);
+    Ent_SET(flags, wall_right, EFLAGS_TOUCHABLE | EFLAGS_SOLID);
+    Ent_SET(position, wall_right, &position);
+    Ent_SET(bounds_width,wall_right, 1);
+    Ent_SET(bounds_height, wall_right, 250);
+    Ent_CALL(spawn, wall_right);
 
     boat = ENT_CREATE(Boat);
-    boat2 = ENT_CREATE(Boat);
     Ent_CALL(spawn, boat);
-    Ent_CALL(spawn, boat2);
+
+    boat2 = ENT_CREATE(Boat);
 
     {
     vec2 pos = {250, 250};
     Ent_SET(position, boat2, &pos);
     }
 
-    video_info = SDL_GetVideoInfo();
+    Ent_CALL(spawn, boat2);
+    Ent_SET(max_speed, boat2, 0.1f);
+}
 
-    for(;;) {
-        frame_start = SDL_GetTicks();
-        if(frame_end == 0)
-            frame_end = frame_start;
-
-        game_time_update(frame_start - frame_end);
-        
-        {
-        SDL_Event event;
-        while(SDL_PollEvent(&event)) {
-            if(event.type == SDL_QUIT)
-                goto done;
-            else if(event.type == SDL_KEYDOWN) {
-                if(event.key.keysym.sym == SDLK_ESCAPE)
-                    goto done;
-            }
-        }
-        }
-        
-        {
-        int col, row;
-        for(col = 0; col < (video_info->current_w / 64) + 1; col++) {
-            for(row = 0; row < (video_info->current_h / 64) + 1; row++) {
-                SDL_Rect pos = {col * 64, row * 64, 0, 0};
-                SDL_BlitSurface(background, NULL, screen, &pos);
-            }
-        }
-        }
-
-        if(game_time() >= Ent_GET(next_think, boat))
-            Ent_CALL(think, boat);
-
-        if(game_time() >= Ent_GET(next_think, boat2))
-            Ent_CALL(think, boat2);
-
-        Ent_CALL(on_frame, boat);
-        Ent_CALL(on_frame, boat2);
-
-        SDL_Flip(screen);
-
-        {
-        Uint32 delay = 1000 / FRAME_RATE_MAX, elapsed;
-
-        frame_end = SDL_GetTicks();
-        elapsed = frame_end - frame_start;
-
-        if(elapsed < delay)
-            delay -= elapsed;
-
-        frame_end = SDL_GetTicks();
-        game_time_update(elapsed);
-        SDL_Delay(delay);
+int check_SDL_events()
+{
+    SDL_Event event;
+    while(SDL_PollEvent(&event)) {
+        if(event.type == SDL_QUIT)
+            return 1;
+        else if(event.type == SDL_KEYDOWN) {
+            if(event.key.keysym.sym == SDLK_ESCAPE)
+                return 1;
         }
     }
 
-done:
-    ENT_FREE(boat);
-    ENT_FREE(boat2);
+    return 0;
+}
 
+
+void
+draw_background(SDL_Surface *background,
+                SDL_Surface *screen,
+                const SDL_VideoInfo *video_info)
+{
+    int col, row;
+    for(col = 0; col < (video_info->current_w / 64) + 1; col++) {
+        for(row = 0; row < (video_info->current_h / 64) + 1; row++) {
+            SDL_Rect pos = {col * 64, row * 64, 0, 0};
+            SDL_BlitSurface(background, NULL, screen, &pos);
+        }
+    }
+}
+
+void
+update_entities(Uint32 last_frame_time)
+{
+    const list_t *ent_iter = NULL;
+
+    /* Ent updating follows, in 3 stages:
+        * -First stage: call the think functions on any entities necessary
+        * -Second stage: update position, angles and bounds for all entities
+        * -Third stage: call frame callbacks, which check collisions and draw to surface
+        */
+    while((ent_iter = ent_table_next(ent_iter)) != NULL) {
+        Ent *ent = ent_iter->item;
+        if(game_time() >= Ent_GET(next_think, ent))
+            Ent_CALL(think, ent);
+    }
+
+    while((ent_iter = ent_table_next(ent_iter)) != NULL) {
+        Ent *ent = ent_iter->item;
+        Ent_update_pre_frame(ent, last_frame_time);
+    }
+
+    Ent_check_all_collisions();
+
+    while((ent_iter = ent_table_next(ent_iter)) != NULL) {
+        Ent *ent = ent_iter->item;
+        Ent_CALL(on_frame, ent, last_frame_time);
+    }
+}
+
+void
+start_frame(Uint32 *frame_start, Uint32 *frame_end)
+{
+    *frame_start = SDL_GetTicks();
+    game_time_update(*frame_start - *frame_end);
+}
+
+void
+finish_frame(Uint32 *frame_start, Uint32 *frame_end)
+{
+    Uint32 delay = 1000 / FRAME_RATE_MAX, elapsed;
+
+    *frame_end = SDL_GetTicks();
+    elapsed = *frame_end - *frame_start;
+
+    if(elapsed < delay)
+        delay -= elapsed;
+
+    *frame_end = SDL_GetTicks();
+    game_time_update(elapsed);
+    SDL_Delay(delay);
+}
+
+int main(int argc, char **argv)
+{
+    SDL_Surface *screen, *background;
+    const SDL_VideoInfo *video_info;
+    const asset_data_t *bg_asset;
+    Uint32 frame_start, frame_end = 0;
+
+    systems_init();
+
+    screen = SDL_SetVideoMode(800, 600, 0, SDL_HWSURFACE|SDL_DOUBLEBUF);
+    video_info = SDL_GetVideoInfo();
+
+    bg_asset = assets_load(ASSET_TYPE_SURFACE, "images/water.tga");
+    background = bg_asset->surface;
+    
+    create_entities();
+ 
+    for(;;) {
+        start_frame(&frame_start, &frame_end);
+
+        if(check_SDL_events())
+            break;
+
+        draw_background(background, screen, video_info);
+
+        update_entities(frame_end);
+
+        SDL_Flip(screen);
+
+        finish_frame(&frame_start, &frame_end);
+    }
+
+done:
+    systems_shutdown();
     return 0;
 }

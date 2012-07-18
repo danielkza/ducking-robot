ca5baff0f66ecb69e87c7f794ffa1c300ac9e31b
Daniel Miranda <danielkza2@gmail.com>
2012-07-17 20:35:24 -0400

    Finished entity table system

--- game/ent_table.c
+++ game/ent_table.c
@@ -1,68 +1,110 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "Ent.h"
-#include "hash_fnv1a.h"
 #include "list.h"
+#include "Ent.h"
 
 #include "ent_table.h"
 
-typedef struct {
-    const ent_class_t* eclass;
-    int ent_count;
-    list_t *ent_list;
-    int ent_free_count;
-    list_t *ent_free_list;
-} ent_table_entry_t;
+static list_t *ent_list;
+static int ent_count;
 
-static ent_table_entry_t ent_table[ENT_TABLE_CLASS_MAX];
-
-static int ent_table_class_count;
-static int ent_table_count;
-
-int
+void
 ent_table_init()
 {
-    ent_table_class_count = 0;
-    ent_table_count = 0;
-}
-
-static void
-ent_table_entry_free(ent_table_entry_t *entry)
-{
-    list_t *iter;
-    Ent *ent;
-    
-    if(entry != NULL) {
-        if(entry->ent_list != NULL) {
-            list_for_each_item(entry->ent_list, iter, ent)
-                ENT_FREE(ent);
-        }
-
-        if(entry->ent_free_list != NULL) {
-            list_for_each_item(entry->ent_free_list, iter, ent)
-                ENT_FREE(ent);
-        }
-    }
+    ent_list = list_create();
+    ent_count = 0;
 }
 
 void
 ent_table_shutdown()
 {
-    int i;
-    for(i = 0; i < ent_table_class_count; i++)
-        ent_table_entry_free(&ent_table[i]);
+    list_t *iter;
+    Ent *ent;
 
-    ent_table_class_count = -1;
-    ent_table_count = -1;
+    list_for_each_item(ent_list, iter, ent) {
+        ENT_FREE(ent);
+    }
+
+    list_free(ent_list);
+    ent_count = -1;
 }
 
-static unsigned int
-ent_table_hash_class(const ent_class_t *eclass)
+void
+ent_table_insert(Ent *ent)
 {
-    hash_fnv1a(&eclass, sizeof(eclass))
+    list_t *iter;
+    Ent *existing_ent;
 
+    list_for_each_item(ent_list, iter, existing_ent) {
+        if(existing_ent == ent)
+            return;
+    }
 
+    list_push(ent_list, ent);
+    ent_count++;
+}
 
+void
+ent_table_remove(Ent *ent)
+{
+    list_t *iter, *next;
 
-'
+    list_for_each_node(ent_list, iter, next) {
+        Ent *existing_ent = iter->item;
+        if(existing_ent == ent) {
+            list_remove(iter);
+            ent_count--;
+        }
+    }
+}
+
+const list_t *
+ent_table_next(const list_t *prev)
+{
+    if(prev == NULL) {
+        if(ent_list == NULL)
+            return NULL;
+
+        return ent_list->next;
+    }
+
+    if(prev->next == ent_list)
+        return NULL;
+
+    return prev->next;
+}
+
+const list_t *
+ent_table_next_by_class(const list_t *prev, 
+                        const ent_class_t *eclass,
+                        int find_subclasses)
+{
+    const list_t *iter;
+    Ent *ent;
+
+    if(eclass == NULL)
+        return NULL;
+    
+    if(prev == NULL)
+        iter = ent_list->next;
+    else
+        iter = prev->next;
+
+    while(iter != ent_list) {
+        ent = iter->item;
+
+        if(!find_subclasses ? (ent->eclass == eclass) : ent_class_is_subclass(ent->eclass, eclass))
+            return iter;
+    
+        iter = iter->next;
+    }
+
+    return NULL;
+}
+
+int
+ent_table_count()
+{
+    return ent_count;
+}
--- game/ent_table.h
+++ game/ent_table.h
@@ -1,7 +1,105 @@
 #ifndef ENT_TABLE_H
 #define ENT_TABLE_H
 
-#define ENT_TABLE_CLASS_MAX 1024
+#include "list.h"
+#include "Ent.h"
 
+/**
+ * Initializes the entity table.
+ * Must be called before using any other function.
+ **/
+void
+ent_table_init();
+
+/**
+ * Destroys the entity table, __freeing all registered entities__.
+ * All existing entity references __are invalidated__.
+ * All existing entity iterators __are invalidated__.
+ *
+ * @warning Does not do reference counting. All data is immediately freed.
+ **/
+void
+ent_table_shutdown();
+
+/**
+ * Inserts an entity into the table.
+ * No provision for avoiding duplicate insertion is provided.
+ *
+ * @param ent Entity to register
+ **/
+void
+ent_table_insert(Ent *ent);
+
+/**
+ * Removes a previously-registered entity from the table.
+ *
+ * @warning This function is automatically called from Ent destructors.
+ *
+ * @param ent Entity to remove.
+ **/
+void
+ent_table_remove(Ent *ent);
+
+/**
+ * Returns an iterator to the first or next entity on the table.
+ *
+ * @param prev Previous entity iterated upon. If NULL, the function will 
+ *             retrieve the first entity.
+ *
+ * @returns Next entity iterator, or NULL if no more are available
+ *
+ * Example of use:
+ * @code
+
+ * const list_t *iter = NULL;
+ * while((iter = ent_table_next(iter)) != NULL) {
+ *     Ent *ent = iter->item;
+ *     printf("ent class: %s\n", ent->eclass->name);
+ * }
+ *
+ * @endcode
+ **/
+const list_t *
+ent_table_next(const list_t *prev);
+
+
+/**
+ * Returns an iterator to the first or next entity on the table which matches
+ * a particular class type.
+ *
+ * @param prev            Previous entity iterated upon. If NULL, the function will 
+ *                        retrieve the first entity.
+ * @param eclass          Entity class to retrieve.
+ * @param find_subclasses If 0, only exact matches of eclass will be accepted.
+ *                        Otherwise, any entities which are objects of
+ *                        subclasses of eclass will also be retrieved.
+ *
+ * @returns Next entity iterator, or NULL if no more are available
+ *
+ * Example of use:
+ * @code
+
+ * const list_t *iter = NULL;
+ * while((iter = ent_table_next_by_class(iter, &Boat_CLASS, 1)) != NULL) {
+ *     Boat *boat = iter->item;
+ *     printf("%s is a Boat!\n", boat->eclass->name);
+ * }
+ *
+ * @endcode
+ **/
+
+const list_t *
+ent_table_next_by_class(const list_t *prev, 
+                        const ent_class_t *eclass,
+                        int find_subclasses);
+
+/**
+ * Retrieves the current count of registed entities on the table.
+ *
+ * @returns Count, or -1 if table is not initialized
+ **/
+
+int
+ent_table_count();
 
 #endif
